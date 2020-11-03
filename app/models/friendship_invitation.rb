8968d8e53f16b1f2d5e9b465e46761acbcd10614
Morrism1 <morrismontana0@gmail.com>
2020-11-03 13:28:34 +0200

    Add friendship invitation controller

--- app/models/friendship_invitation.rb
+++ app/models/friendship_invitation.rb
@@ -1,3 +1,7 @@
 class FriendshipInvitation < ApplicationRecord
   belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  validates_presence_of :user_id, :friend_id, message: "can't be blank"
+  validates_uniqueness_of :user_id, scope: [:friend_id]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,22 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendship_invitations
+  has_many :friendship_invitations, foreign_key: 'user_id'
+  has_many :friends, through: :friendship_invitations, foreign_key: 'friend_id'
   has_many :pending_invitation, -> { where status: false }, class_name: 'FriendshipInvitation', foreign_key: 'friend_id'
+
+  def friends
+    sent_invitation = FriendshipInvitation.where(user_id, status: true).pluck(:friend_id)
+    got_invitation = FriendshipInvitation.where(friend_id, status: true).pluck.call(:user_id)
+    fr_array = sent_invitation + got_invitation
+    User.where(id: fr_array)
+  end
+
+  def friends?(user)
+    friends.includes(user)
+  end
+
+  def sent_invitation(user)
+    FriendshipInvitation.create(friend_id: user.id)
+  end
 end
