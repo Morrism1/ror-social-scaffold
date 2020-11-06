700db78af3cc91b1c1d31e6aaf762d1a6fc48b08
Morrism1 <morrismontana0@gmail.com>
2020-11-07 00:55:59 +0200

    Remove logic from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,29 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def left_menu_link
+    class_name = 'menu-item'
+    if current_user
+      (content_tag(:div, class: class_name) do
+        menu_link_to('Friend Requests', user_friendships_path(current_user.id))
+      end) +
+        link_to('Sign out', destroy_user_session_path, method: :delete)
+    else
+      link_to('Sign in', user_session_path)
+    end
+  end
+
+  def bootstrap_class_for flash_type
+    { success: "success", error: "danger", alert: "alert", notice: "notice" }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "#{bootstrap_class_for(msg_type)}", role: "alert") do 
+              concat message 
+            end)
+    end
+    nil
+  end
 end
