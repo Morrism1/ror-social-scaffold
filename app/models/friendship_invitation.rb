5b93ab229f5495c0a51021136be300d1704751fd
Morrism1 <morrismontana0@gmail.com>
2020-11-09 14:17:31 +0200

    Validate duplicate friendships and self invitations for friendships

--- app/models/friendship_invitation.rb
+++ app/models/friendship_invitation.rb
@@ -1,7 +1,24 @@
+class DuplicateValidator < ActiveModel::Validator
+  def validate(record)
+    friend1 = FriendshipInvitation.find_by(user_id: record.user_id, friend_id: record.friend_id)
+    friend2 = FriendshipInvitation.find_by(friend_id: record.user_id, user_id: record.friend_id)
+    record.errors[:base] << 'This relationship already exists' unless friend1.nil? && friend2.nil?
+  end
+end
+
+class SelfInvitationValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:base] << "You can't be friends with yourself" if record.user_id == record.friend_id
+  end
+end
+
 class FriendshipInvitation < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
+  validates_with DuplicateValidator
+  validates_with SelfInvitationValidator
 
   validates_presence_of :user_id, :friend_id, message: "can't be blank"
   validates_uniqueness_of :user_id, scope: [:friend_id]
+
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
 end
